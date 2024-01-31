e45e467f552a61b471cf40eb271c5b6aeccd645e
Markus Högberg <m.o.hogberg@gmail.com>
2024-01-31 09:55:41 +0000

    Add add task functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,9 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-});
 
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.modal');
-    M.Modal.init(elems);
+    //datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
+    // modal initialization
+    let showModal = document.querySelectorAll('.modal');
+    M.Modal.init(showModal);
 });
